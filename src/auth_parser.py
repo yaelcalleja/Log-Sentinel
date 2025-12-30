888a48cd4525204e5a9afbbe603f3e396ca0b46d
badger <yael.calleja321@gmail.com>
2025-12-29 20:35:07 -0600

    Regex added so now can find important data on a linux server log

--- src/auth_parser.py
+++ src/auth_parser.py
@@ -1,3 +1,16 @@
 """
 Class for log files such as auth.log (Linux server)
 """
+
+import log_parser  # The father class and engine of the research for the code.
+
+
+class AuthParser(log_parser.LogEntry):  # Inherated the methods from the father class
+
+    _Regexs = {  # We only incorporate the regex because the research methods becomes from the father class.
+        "_Date": r"^[A-Z]{1}[a-z]{2} \d{2} \d{2}:\d{2}:\d{2}",
+        "_User": r"user ([a-z]+)|([a-z]+) from",
+        "_Ip": r"((?:\d{1,3}\.){3}\d{1,3})",
+        "_Port": r"port (\d+)",
+        "_Service": r"([a-z]{1,6}[1-9]{1,5})$",
+    }
